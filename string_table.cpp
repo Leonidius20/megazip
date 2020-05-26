cdcf640cbdbd95e00442689de4f220c77f06fcab
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-05-26 15:44:17 +0300

    fixed end-of-file code in string table

--- string_table.cpp
+++ string_table.cpp
@@ -1,23 +1,21 @@
 #include <string>
 #include <iostream>
-#include <limits>
+#include <climits>
 #include <stdexcept>
 #include "code.h"
 #include "string_table.h"
 
 using namespace std;
 
-const unsigned char MAX_UCHAR = numeric_limits<unsigned char>::max();
-
-StringTable::StringTable() : lastCode(MAX_UCHAR) {
-    for (unsigned char b = 0; b < MAX_UCHAR; ++b) {
+StringTable::StringTable() : lastCode(UCHAR_MAX) {
+    for (unsigned char b = 0; b < UCHAR_MAX; ++b) {
         table[string(1, b)] = Code(b);
     }
-    table[string(1, MAX_UCHAR)] = Code(MAX_UCHAR);
+    table[string(1, UCHAR_MAX)] = Code(UCHAR_MAX);
 }
 
 bool StringTable::addCode(const string &key) {
-    if (lastCode.toShort() == 0xFFF) {
+    if (lastCode.toShort() == Code::END_OF_FILE.toShort() - 1) {
         return false; // table is full
     }
     table[key] = ++lastCode;
